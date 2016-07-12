6fadca1883cf2a99669a67779a49d8d608b4d570
CaryZheng <CaryZhengMy@gmail.com>
2016-07-12 15:13:56 +0800

    Add API: get_city_list

--- Server/Project/ZWeatherServer/Package.swift
+++ Server/Project/ZWeatherServer/Package.swift
@@ -3,8 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporApp",
     dependencies: [
-        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 11),
-		.Package(url: "https://github.com/qutheory/vapor-mustache.git", majorVersion: 0, minor: 7)
+        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 13)
     ],
     exclude: [
 	    "Config",
