2e02463167a09b835cd6120184bed73d3f08014a
CaryZheng <CaryZhengMy@gmail.com>
2016-07-28 09:55:45 +0800

    Add API: get weather by geographic coordinates

---
+++ ZWeatherServer/App/Configure/StaticConfigure.swift
@@ -0,0 +1,8 @@
+
+class StaticConfigure {
+	private static let OriginalDataURL = "http://api.openweathermap.org/data/2.5"
+    
+    static func getOriginalDataURL() -> String {
+        return OriginalDataURL
+    }
+}
--- ZWeatherServer/App/Handler/RequestHandler.swift
+++ ZWeatherServer/App/Handler/RequestHandler.swift
@@ -1,19 +1,28 @@
 import Vapor
 
 class RequestHandler {
-    static func handleGetCityList(request: Vapor.Request) throws -> ResponseRepresentable{
+    static func handleGetCityList(request: Vapor.Request) throws -> ResponseRepresentable {
         let cityListData = CityListDataManager.getInstance().getCityListData()
         
         return Response(status: .ok, body: .data(cityListData))
 	}
     
-    static func handleGetCurrentWeather(request: Vapor.Request) throws -> ResponseRepresentable{
-        guard let cityID = request.data["id"]?.string else {
-            return ErrorResponseUtility.getErrorResponse(errorType: ErrorResponseType.ERROR_PARAM)
+    static func handleGetCurrentWeather(request: Vapor.Request) throws -> ResponseRepresentable {
+        let cityID = request.data["id"]?.string
+        if let cityID = cityID {
+            let response = try app.client.get(StaticConfigure.getOriginalDataURL()+"/weather", query: ["id": cityID, "appid": OpenWeatherMapAPIKey])
+            
+            return response
         }
         
-        let response = try app.client.get("http://api.openweathermap.org/data/2.5/weather", query: ["id": cityID, "appid": OpenWeatherMapAPIKey])
+        let lat = request.data["lat"]?.double
+        let lon = request.data["lon"]?.double
+        if let lat = lat, let lon = lon {
+            let response = try app.client.get(StaticConfigure.getOriginalDataURL()+"/weather", query: ["lat": lat, "lon": lon, "appid": OpenWeatherMapAPIKey])
+            
+            return response
+        }
         
-        return response
+        return ErrorResponseUtility.getErrorResponse(errorType: ErrorResponseType.ERROR_PARAM)
     }
 }
